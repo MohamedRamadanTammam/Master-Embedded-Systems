22520ad4d868670d41f8bd4eed59d56dd44e23f4
Mohamed ramadan tammam <144618615+MohamedRamadanTammam@users.noreply.github.com>
2023-10-06 15:07:26 +0300

    Add files via upload

---
+++ home_work1(ex1).c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+
+ Name        : home_work1(ex1).c
+ Author      : mohamed
+ Version     :
+ Copyright   : home work 1 (ex1)
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+
+	printf ("C Programming");
+	return 0;
+
+}
